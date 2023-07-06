9d1cad422d146954c326a63a57f299fe0d1b08de
notenoughnowjy <jyp4188@gmail.com>
2023-07-06 11:20:38 +0900

    [2023.07.06] / Recap(06:09)

--- webtoonapplication/lib/models/webtoon_model.dart
+++ webtoonapplication/lib/models/webtoon_model.dart
@@ -2,8 +2,18 @@ class WebtoonModel {
   final String title, thumb, id;
 
 //named constructor
+//map은 object처럼 Dart가 지원하는 key-value 데이터 구조이다.
   WebtoonModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         thumb = json['thumb'],
         id = json['id'];
+
+//   late final String title, thumb, id;
+// //이 코드에서는 앞의 webtoonInstances.add(WebtoonModel.fromJson(webtoon)); 부분에서 fromJson만 지워주면 된다.
+// // 하지만 웹툰 Json으로 WebtoomModel 인스턴스를 만드는게 전부임.
+//   WebtoonModel.fromJson(Map<String, dynamic> json) {
+//     title = json['title'];
+//     thumb = json['thumb'];
+//     id = json['id'];
+//   }
 }
--- webtoonapplication/lib/services/api_service.dart
+++ webtoonapplication/lib/services/api_service.dart
@@ -13,13 +13,16 @@ class ApiService {
   Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
+    //response가 반환되는지 확인
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      //리스트 webtoons에 json으로 반환되는 resposer.body를 넣어주기
-      final List<dynamic> webtoons = jsonDecode(response.body);
+      //webtoons에 json으로 반환되는 resposer.body를 넣어주기
+      final webtoons = jsonDecode(response.body);
+      // for(var webtoon in jsonDecode(response.body))도 가능하다.
       //리스트를 하나하나 출력
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
