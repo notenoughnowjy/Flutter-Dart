a871b443576b75f9f085da7800eca25c4a26fbb5
notenoughnowjy <jyp4188@gmail.com>
2023-07-06 15:19:37 +0900

    [2023.07.06] / #6.8 Webtoon Card(13:55)

--- webtoonapplication/lib/screens/home_screen.dart
+++ webtoonapplication/lib/screens/home_screen.dart
@@ -2,34 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:webtoonapplication/models/webtoon_model.dart';
 import 'package:webtoonapplication/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  //const는 컴파일 전에 값을 알고 있다는 뜻.
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoonsList = [];
-  bool isLoading = true;
-
-//http 요청을 기다리는 함수를 기다려야 한다. => 비동기 프로그래밍
-  void waitForWebToons() async {
-    webtoonsList = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
-
+  Future<List<WebtoonModel>> webtoonList = ApiService.getTodaysToons();
   @override
   Widget build(BuildContext context) {
-    print(webtoonsList);
-    print(isLoading);
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
@@ -42,6 +21,69 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.w400,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoonList,
+        builder: (context, snapshot) {
+          // snapshot을 이용해 Future의 상태 확인
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 5,
+                        offset: const Offset(10, 8),
+                        color: Colors.red.withOpacity(0.5))
+                  ]),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
