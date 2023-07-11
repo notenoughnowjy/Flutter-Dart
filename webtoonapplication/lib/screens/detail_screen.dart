2e01ff55d1ab962b3cc4ae188afac69af0cff06e
notenoughnowjy <jyp4188@gmail.com>
2023-07-11 14:52:03 +0900

    [2023.07.11] / #6.13 Futures(07:48)

--- webtoonapplication/lib/screens/detail_screen.dart
+++ webtoonapplication/lib/screens/detail_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:webtoonapplication/models/webtoon_detail_model.dart';
+import 'package:webtoonapplication/models/webtoon_episode_model.dart';
+import 'package:webtoonapplication/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
   const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +36,7 @@ class DetailScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           foregroundColor: Colors.blue,
           title: Text(
-            title,
+            widget.title,
             style: const TextStyle(
               fontSize: 27,
               fontWeight: FontWeight.w400,
@@ -32,7 +52,7 @@ class DetailScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Hero(
-                  tag: id,
+                  tag: widget.id,
                   child: Container(
                     width: 250,
                     clipBehavior: Clip.hardEdge,
@@ -47,7 +67,7 @@ class DetailScreen extends StatelessWidget {
                       ],
                     ),
                     child: Image.network(
-                      thumb,
+                      widget.thumb,
                       headers: const {
                         "User-Agent":
                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
--- webtoonapplication/lib/services/api_service.dart
+++ webtoonapplication/lib/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoonapplication/models/webtoon_detail_model.dart';
 import 'package:webtoonapplication/models/webtoon_episode_model.dart';
 import 'package:webtoonapplication/models/webtoon_model.dart';
 
@@ -30,6 +31,17 @@ class ApiService {
     throw Error();
   }
 
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+// Url fetch
   static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
       String id) async {
     List<WebtoonEpisodeModel> episodesInstances = [];
