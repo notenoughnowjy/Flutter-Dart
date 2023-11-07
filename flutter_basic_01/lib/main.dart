fbf49f7837a8ebbd4c3e73c59dc772898c741907
ParkJunYeong <jyp4188@gmail.com>
2023-11-07 22:37:17 +0900

    [2023.11.07] flutter appbar updated

--- flutter_basic_01/lib/main.dart
+++ flutter_basic_01/lib/main.dart
@@ -20,48 +20,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      // 저장만 해 변경 내용이 반영되는 것을 Hot_Reload 기능이라고 말함.
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title; //
-  MyHomePage({required this.title});
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello Flutter!'),
-        backgroundColor: Colors.blueGrey,
-        // centerTitle: false,
-        elevation: 99,
-        foregroundColor: Colors.red,
-      ),
-      body: Center(
-        // child: Image.asset('images/tree.jpg'),
-        child: Text(
-          'Hello, Text Widget',
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.purple,
-            backgroundColor: Colors.red,
-          ),
-        ),
+        title: const Text("ANU meal"),
+        titleTextStyle: const TextStyle(color: Colors.black, fontSize: 26),
+        centerTitle: true,
+        backgroundColor: Colors.white,
       ),
     );
   }
